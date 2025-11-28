4bbaa81bef85bc89523ac8b80faae6195b9ef444
AndreyQuantum <zelentsov1254@gmail.com>
2025-11-28 08:48:32 +0100

    secure all API routes (`/student`, `/grade`, `/course`) with `auth_security` dependency

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from api.auth import auth_router
+from api.dependencies.token import auth_security
 from api.student import app as student_router
 from api.grade import app as grade_router
 from api.courses import app as courses_router
@@ -32,7 +33,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(router=auth_router, prefix="/auth",tags=["auth"])
 
-app.include_router(router=student_router, prefix="/student", tags=["student"])
-app.include_router(router=grade_router, prefix="/grade", tags=["grade"])
-app.include_router(router=courses_router, prefix="/course",tags=["course"])
+app.include_router(router=student_router, prefix="/student", tags=["student"], dependencies=[auth_security])
+app.include_router(router=grade_router, prefix="/grade", tags=["grade"], dependencies=[auth_security])
+app.include_router(router=courses_router, prefix="/course",tags=["course"], dependencies=[auth_security])
 app.include_router(router=user_app, prefix="/user",tags=["user"])
